cc8286784dfd332b4c31b3ebdd85c5f6c52e71e5
CodexX <codexx@CodexXs-Laptop.local>
2024-07-25 08:41:27 +0530

    debounce hook added, timer and range components added

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export const Timer = () => {
-  return (
-    <div>Timer</div>
-  )
-}
+export const Timer = ({ isActive = false }) => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  return <div className="text-white text-center">{time}s</div>;
+};
--- src/pages/SortVisualizer.jsx
+++ src/pages/SortVisualizer.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DropDown } from "../components/DropDown";
+import { Timer } from "../components/Timer";
+import { Range } from "../components/Range";
 
 let list = [
   "Bubble Sort",
@@ -13,23 +15,70 @@ let list = [
   "Bucket Sort",
 ];
 
+const generateArray = (number) => {
+  //random order of numbers 1- number
+  let arr = [];
+  for (let i = 1; i <= number; i++) {
+    arr.push(i);
+  }
+  arr.sort(() => Math.random() - 0.5);
+  console.log(arr);
+  return arr;
+};
+
 export const SortVisualizer = () => {
+  const [array, setArray] = useState([]);
   const [technique, setTechnique] = useState("Bubble Sort");
+  const [timerState, setTimerState] = useState(false);
+
+  const createArray = (number) => {
+    setArray(generateArray(number));
+  };
+
   console.log(technique);
   return (
-    <div className="h-24 bg-red-500 flex flex-row items-center px-16">
-      <div className="w-[20%]">
-        <h4 className="text-white font-thin">Algorithm</h4>
-        <DropDown
-          defaultValue={technique}
-          handleChange={(value) => {
-            // console.log(value);
-            setTechnique(value);
-          }}
-          list={list}
-        />
+    <div className="h-24 bg-red-500 flex flex-row justify-between px-16">
+      <div className="w-[80%] flex flex-row items-center gap-x-14">
+        <div className="w-[20%]">
+          <h4 className="text-white font-thin">Algorithm</h4>
+          <DropDown
+            defaultValue={technique}
+            handleChange={(value) => {
+              // console.log(value);
+              setTechnique(value);
+            }}
+            list={list}
+          />
+        </div>
+        <div className="">
+          <h4 className="text-white font-thin">Execution Time</h4>
+          <Timer isActive={timerState} />
+        </div>
+
+        <div>
+          <button
+            className="bg-transparent text-white text-sm hover:underline underline-offset-2 transition-all ease-in-out duration-800"
+            onClick={() => {
+              setTimerState(false);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+        <div>
+          <Range createArray={createArray} timerState={timerState} />
+        </div>
       </div>
-      
+      <div className="flex items-center">
+        <button
+          className="bg-yellow-500 text-white px-4 py-2 rounded-[1px] hover:bg-white hover:text-black transition-all ease-in-out duration-500"
+          onClick={() => {
+            setTimerState(!timerState);
+          }}
+        >
+          {timerState ? "Stop" : "Start"}
+        </button>
+      </div>
     </div>
   );
 };
